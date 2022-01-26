e14e4ff12cbd0ad60c1a8e752d9629796b20a9f5
Pankaj Kumar <pankaj290h@gmail.com>
2022-01-26 17:28:05 +0530

    v2 of portfolio website

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-// const s1 = document.querySelector("section1-text");
+const heading = document.querySelector(".h1");
+const sub_title = document.querySelector(".h3");
+const circle = document.querySelector(".circle");
+const nav = document.querySelector(".nav");
+gsap.from(heading, { y: 300, duration: 2 });
+gsap.from(sub_title, { y: 200, duration: 2, delay: 1.5 });
 
-gsap.from(".section1-text", { opacity: 0, x: -200, duration: 2 });
+gsap.from(circle, {
+  opacity: 0,
+  rotate: -90,
+  duration: 5,
+});
+gsap.from(nav, { y: -150, duration: 1, delay: 1.5 });
